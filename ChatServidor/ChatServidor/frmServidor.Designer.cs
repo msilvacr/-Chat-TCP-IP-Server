4100759a1b0ef908bfed12878860b576e876a379
Marlon Cruz <marlomsilvacruz@gmail.com>
2020-05-22 00:00:36 -0300

    Sei lá oq eu fiz agr

--- ChatServidor/ChatServidor/frmServidor.Designer.cs
+++ ChatServidor/ChatServidor/frmServidor.Designer.cs
@@ -37,38 +37,35 @@
             // 
             // txtLog
             // 
-            this.txtLog.Location = new System.Drawing.Point(281, 85);
-            this.txtLog.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLog.Location = new System.Drawing.Point(211, 69);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(519, 281);
+            this.txtLog.Size = new System.Drawing.Size(390, 229);
             this.txtLog.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 85);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(11, 69);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.Size = new System.Drawing.Size(17, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "IP";
             // 
             // txtIP
             // 
-            this.txtIP.Location = new System.Drawing.Point(18, 110);
-            this.txtIP.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIP.Location = new System.Drawing.Point(14, 89);
             this.txtIP.Name = "txtIP";
-            this.txtIP.Size = new System.Drawing.Size(237, 22);
+            this.txtIP.Size = new System.Drawing.Size(179, 20);
             this.txtIP.TabIndex = 2;
             // 
             // btnAtender
             // 
-            this.btnAtender.Location = new System.Drawing.Point(18, 155);
-            this.btnAtender.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAtender.Location = new System.Drawing.Point(14, 121);
             this.btnAtender.Name = "btnAtender";
-            this.btnAtender.Size = new System.Drawing.Size(165, 28);
+            this.btnAtender.Size = new System.Drawing.Size(179, 23);
             this.btnAtender.TabIndex = 3;
             this.btnAtender.Text = "Start Server";
             this.btnAtender.UseVisualStyleBackColor = true;
@@ -79,25 +76,25 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(9, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(284, 31);
+            this.label2.Size = new System.Drawing.Size(230, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "TCP/IP CHAT APPLICATION";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(819, 388);
+            this.ClientSize = new System.Drawing.Size(614, 315);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnAtender);
             this.Controls.Add(this.txtIP);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtLog);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Chat Servidor";
             this.ResumeLayout(false);
